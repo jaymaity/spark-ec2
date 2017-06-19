23345ffad10760938fd1ec381baa336056de3aa2
Jay Maity <jaymaity@gmail.com>
2017-06-19 15:26:54 -0700

    Add script to run spark job automatically

--- setup.sh
+++ setup.sh
@@ -119,6 +119,7 @@ for module in $MODULES; do
 
 done
 
-/root/spark/bin/spark-submit --master $MASTERS /root/task/test_sample_spark.py 
+# run automated spark script
+/root/spark/bin/spark-submit --master "spark://$MASTERS:7077" /root/task/test_sample_spark.py
 
 popd > /dev/null
