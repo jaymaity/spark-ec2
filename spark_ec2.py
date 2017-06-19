c98060039ab68808a60eda3b0c2f675de9537559
Jay Maity <jaymaity@gmail.com>
2017-06-19 11:55:25 -0700

    EXperiment by removing scala, rsstudio and stantalone spark

--- spark_ec2.py
+++ spark_ec2.py
@@ -104,7 +104,7 @@ DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
 DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
-DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/amplab/spark-ec2"
+DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/jaymaity/spark-ec2"
 DEFAULT_SPARK_EC2_BRANCH = "branch-1.6"
 
 
@@ -811,7 +811,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key):
             ssh_write(slave_address, opts, ['tar', 'x'], dot_ssh_tar)
 
     modules = ['spark', 'ephemeral-hdfs', 'persistent-hdfs',
-               'mapreduce', 'spark-standalone', 'tachyon', 'rstudio']
+               'mapreduce', 'tachyon']
 
     if opts.hadoop_major_version == "1":
         modules = list(filter(lambda x: x != "mapreduce", modules))
