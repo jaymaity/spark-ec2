448dd93a98820c05f9462c9bd4c733cc29cd8831
Jay Maity <jaymaity@gmail.com>
2017-06-27 16:17:42 -0700

    Yum ypdate y

--- task/setup-master.sh
+++ task/setup-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum update
 sudo /root/spark-ec2/copy-dir /root/task
 sudo chmod 777 /root/task
+sudo yum -y update
 sudo /root/spark/bin/spark-submit --master "spark://$MASTERS:7077" /root/task/test_sample_spark.py
--- task/setup-slave.sh
+++ task/setup-slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -y yum update
 sudo /root/spark-ec2/copy-dir /root/task
+sudo yum -y update
