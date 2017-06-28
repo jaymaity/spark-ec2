239d873c7be1503c5801d989b84c5d7a7c85dbcf
Jay Maity <jaymaity@gmail.com>
2017-06-28 08:44:55 -0700

    Removal of all hdfs dependency

--- templates/root/task/collectlog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-DATE=$(date +"%FT%T")
-aws s3 cp $1 s3://$2/logs/$DATE --recursive --exclude "*" --include "*.log"
--- templates/root/task/runloadtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo su
-cd $1
-export LD_LIBRARY_PATH="/root/task/distrib-load-test-app/":
-chmod 777 $1*.*
-echo $(hostname -f)
-$1distrib-load-test-app $(hostname -f) $2
-            
