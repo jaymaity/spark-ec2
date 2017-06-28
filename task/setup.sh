7f9e2391850d8d815c9d4702810a9d8efb96e2b9
Jay Maity <jaymaity@gmail.com>
2017-06-28 10:40:55 -0700

    EXP

--- task/setup.sh
+++ task/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo hostname
-echo pwd
-/root/spark-ec2/copy-dir /root/task
+echo $(hostname -f)
+echo $(pwd)
 
 for node in $SLAVES; do
   echo $node
-  ssh -t -t $SSH_OPTS root@$node "/root/task/setup-slave.sh" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "/root/spark-ec2/task/setup-slave.sh" & sleep 0.3
   echo $node
 done
 wait
@@ -13,7 +12,7 @@ wait
 for node in $MASTERS; do
   echo $node
 
-  ssh -t -t $SSH_OPTS root@$node "/root/task/setup-master.sh" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "/root/spark-ec2/task/setup-master.sh" & sleep 0.3
   echo $node
 done
 wait
